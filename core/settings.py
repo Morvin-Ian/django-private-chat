4b40a1be8ee0f826099ce01fe0d7b494f4b4bc0b
OluochIan <morvinian@gmail.com>
2024-09-14 19:43:08 +0300

    frontend changes: editing stores

--- core/settings.py
+++ core/settings.py
@@ -177,7 +177,7 @@ STATIC_ROOT = '/app/staticfiles'
 CORS_ORIGIN_ALLOW_ALL = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,' app', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
